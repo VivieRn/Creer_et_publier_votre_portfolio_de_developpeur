4b96d62cdc61c78d7bb2a529d50214f837cde93f
Nicolas VIVIER <vivier.nicolas0@gmail.com>
2023-09-13 10:55:20 +0200

    Ajout du projet Kasa a la partie Travaux

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,5 +1,6 @@
 import "../styles/Works.css";
 import SEO from "../assets/SEO.webp";
+import Kasa from "../assets/kasa-logo.svg";
 
 function Works() {
   return (
@@ -29,20 +30,17 @@ function Works() {
             <p>Gestion du cache sur mesure via Service Worker</p>
           </div>
           <div className="work-card">
-            <h2>Optimisation SEO</h2>
+            <h2>Création React</h2>
             <a
-              href="https://viviern.github.io/Debugger_et_optimiser_un_site_de_photographe/"
+              href="https://viviern.github.io/Kasa_React/"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <img
-                src={SEO}
-                alt="Screen shot du site web optimisé de Nina Carducci."
-              ></img>
+              <img className="kasa" src={Kasa} alt="Logo de Kasa."></img>
             </a>
 
-            <p>Optimisation des photos et de leurs chargements.</p>
-            <p>Gestion du cache sur mesure via Service Worker</p>
+            <p>Création de composants modulable.</p>
+            <p>Gestion efficace des routes</p>
           </div>
           <div className="work-card">
             <h2>Optimisation SEO</h2>
