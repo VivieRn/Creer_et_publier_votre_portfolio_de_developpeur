ea6b30aca2dfbf32494f6b01a8c7634a2df625ac
Nicolas VIVIER <vivier.nicolas0@gmail.com>
2024-01-04 15:19:45 +0100

    Ajout d'une animation pour le h1 du module Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,38 @@
+import React, { useState, useEffect, useRef } from "react";
 import "../styles/Header.css";
 
 function Header() {
+  const titlesRef = useRef([
+    "Développeur Web",
+    "Designer Web",
+    "Nicolas VIVIER",
+  ]);
+  const [currentTitle, setCurrentTitle] = useState("");
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    let currentString = titlesRef.current[index];
+    let displayTitle = "";
+
+    const typingInterval = setInterval(() => {
+      if (displayTitle.length < currentString.length) {
+        displayTitle += currentString[displayTitle.length];
+        setCurrentTitle(displayTitle);
+      } else {
+        clearInterval(typingInterval);
+        setTimeout(() => {
+          setIndex((prevIndex) => (prevIndex + 1) % titlesRef.current.length);
+        }, 4000); // Temps d'attente avant de passer au titre suivant
+      }
+    }, 100); // Vitesse de frappe
+
+    return () => clearInterval(typingInterval);
+  }, [index]);
+
   return (
     <div className="Header-ctn">
       <div className="Header">
-        <h1>Développeur / Designer Web en alternance</h1>
+        <h1 className="animated-title">{currentTitle}</h1>
         <p>Cherche contrat de professionnalisation</p>
       </div>
     </div>
